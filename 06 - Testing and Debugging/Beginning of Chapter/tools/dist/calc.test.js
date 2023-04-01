8b67656ba7f13b200e6c139db2ebb0a224306d04
Reuben Amoako-Atta <yawatta@gmail.com>
2023-04-02 01:30:25 +0800

    Package updates, added git ignore file

---
+++ 06 - Testing and Debugging/Beginning of Chapter/tools/dist/calc.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calc_1 = require("./calc");
+test("check result value", () => {
+    let result = (0, calc_1.sum)(10, 20, 30);
+    expect(result).toBe(60);
+});
